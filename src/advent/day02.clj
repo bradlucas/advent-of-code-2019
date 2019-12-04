5716be79fe6619539197cae4f73a9766a2f06fcd
Brad Lucas <brad@beaconhill.com>
2019-12-04 11:39:50 -0500

    day 2 part 2

--- src/advent/day02.clj
+++ src/advent/day02.clj
@@ -77,3 +77,90 @@
   (part1)
   3716293
 )
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Part 2
+
+;; Accept the preproccing inputs and return result
+;;
+;; What combination of noun verb values produces 19690720
+
+;; https://adventofcode.com/2019/day/2#part2
+
+
+
+;; Tried and found the answer through trial and error
+;; advent.day02> (first (part2 (input) 100 0))
+;; 30 749 891
+;; advent.day02> (first (part2 (input) 50 0))
+;; 15 389 891
+;; advent.day02> (first (part2 (input) 80 0))
+;; 24605891
+;; advent.day02> (first (part2 (input) 60 0))
+;; 18461891
+;; advent.day02> (first (part2 (input) 70 0))
+;; 21533891
+;; advent.day02> (first (part2 (input) 64 29))
+;; 19690720
+;; advent.day02> (first (part2 (input) 64 5))
+;; 19690696
+;; advent.day02> (first (part2 (input) 64 12))
+;; 19690703
+;; advent.day02> (first (part2 (input) 64 24))
+;; 19690715
+;; advent.day02> (first (part2 (input) 64 27))
+;; 19690718
+;; advent.day02> (first (part2 (input) 64 29))
+;; 19690720
+
+
+
+;; Strategy
+;; Increment noun values while leaving verb at 0 
+;; Til you go past the desired value
+;; Then using the previous noun, increment the verb until the value is found
+
+
+(defn prepare [lst noun verb]
+  (assoc (assoc lst 1 noun) 2 verb))
+
+(defn process2 [lst noun verb]
+  (let [lst (prepare lst noun verb)]
+    (first (intcode lst))))
+
+
+(defn part2 [lst goal]
+  (loop [noun 0
+         verb 0
+         noun-loop true    ;; working on nouns, false for verbs
+         ]
+    (let [result (process2 lst noun verb)]
+      (if (= result goal)
+        [noun
+         verb]
+        (let [[new-noun new-verb loop-param] (if (and noun-loop)
+                                              (if (> result goal)
+                                                [(dec noun) (inc verb) false]
+                                                [(inc noun) verb true])
+                                                [noun (inc verb) false])]
+          (recur new-noun new-verb loop-param))))))
+
+
+;; Find the input noun and verb that cause the program to produce the output 19690720. 
+;; What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+
+(defn solve []
+  (let [[noun verb] (part2 (input) 19690720)]
+    (+ (* 100 noun) verb)))
+
+
+
+(comment
+  ;; advent.day02> (solve)
+  [64 29]
+
+  ;; advent.day02> (solve)
+  6429
+)
