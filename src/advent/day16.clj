12be2fb9516089030c6217b86951eaf820377314
Brad Lucas <brad@beaconhill.com>
2020-01-21 10:29:26 -0500

    day 16 part 2

--- src/advent/day16.clj
+++ src/advent/day16.clj
@@ -90,3 +90,47 @@
   (part1)
   ;; => "50053207"
   )
+
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Part2
+
+;; Read the first 7 digits as offset
+;; Repeat input 10000 times
+;; Run 100 phases
+;; Skip that number offset number of digits
+;; Read the next 8 digits
+
+
+;; Used to solve part2
+;; @see https://www.reddit.com/r/adventofcode/comments/ebai4g
+;; @see https://github.com/transducer/adventofcode/blob/master/src/adventofcode/2019/day16.clj
+;; also
+;; @see https://www.reddit.com/r/adventofcode/comments/ebf5cy/2019_day_16_part_2_understanding_how_to_come_up/
+
+
+(defn end-fft [xs]
+  (reductions (fn [acc n] (mod (+ acc n) 10)) xs))
+
+(defn part2 []
+  (let [input (num-str-to-list (input))
+        offset (read-string (apply str (take 7 input)))
+        result (->> input
+                    (repeat 10000)
+                    (apply concat)
+                    (drop offset)
+                    reverse
+                    (iterate end-fft))]
+    (->> (nth result 100)
+         reverse
+         (take 8)
+         list-to-num-str)))
+
+
+
+(comment
+  (part2)
+  ; => "32749588"
+  )
